46e5f01af638c43b25033d05e89cae590f582b17
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-12-01 14:32:50 +0100

    erros updated

---
+++ csv.py
@@ -0,0 +1,2 @@
+# syntax for csv files:
+
--- errors.py
+++ errors.py
@@ -2,11 +2,17 @@
 
 
 try:
-    #some code
+    # code that might cause an error
     print("I'm awsome!")
-except ValueError:
+except ValueError:  
     print("That is not a valid number")
 except ZeroDivisionError:
     print("You can't divide by 0")
 except FileNotFoundError:
     print("There is no such file")
+except TypeError:
+    print("object of type 'int' has no len() OR SOME OTHER MESSAGE")
+except PermissionError:
+    print("You don't have permission!")
+finally:
+    # ALWAYS runs
